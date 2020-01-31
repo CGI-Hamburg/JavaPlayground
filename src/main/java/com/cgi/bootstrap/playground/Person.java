416b3ac95516cba6e0279e772bd91e169794e3dd
Patrick Trapp <patrick.trapp@cgi.com>
2020-01-31 17:56:21 +0100

    added equals

--- src/main/java/com/cgi/bootstrap/playground/Person.java
+++ src/main/java/com/cgi/bootstrap/playground/Person.java
@@ -16,10 +16,25 @@ public class Person extends Object{
                 "age: " + age+ "\n";
     }
 
+    @Override
+    public boolean equals(final Object other) {
+        if(other instanceof Person){
+            Person otherPerson = (Person) other;
+            return  this.name.equals(otherPerson.name) &&
+                    this.age == otherPerson.age;
+        }
+        return false;
+    }
+
     public static void main(String args[]){
         Person person = new Person("Arya Stark", 18);
-
-
+        Person person2 = new Person("Sansa Stark", 20);
+    Person person3 = person;
+        if(person.equals(person2)){
+            System.out.println("Die Personen sind gleich"); // TODO remove
+        }else {
+            System.out.println("Die Personen sind NICHT gleich"); // TODO remove
+        }
         System.out.println(person);
     }
 
