1882124d20830c9f6d36052e0dde2d55fab969cf
Patrick Trapp <patrick.trapp@cgi.com>
2020-01-31 17:19:31 +0100

    added constructor and toString

--- src/main/java/com/cgi/bootstrap/playground/Person.java
+++ src/main/java/com/cgi/bootstrap/playground/Person.java
@@ -1,8 +1,26 @@
 package com.cgi.bootstrap.playground;
 
-public class Person {
-
+public class Person extends Object{
     private String name;
     private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "name: " + name+ "\n"+
+                "age: " + age+ "\n";
+    }
+
+    public static void main(String args[]){
+        Person person = new Person("Arya Stark", 18);
+
+
+        System.out.println(person);
+    }
 
 }
