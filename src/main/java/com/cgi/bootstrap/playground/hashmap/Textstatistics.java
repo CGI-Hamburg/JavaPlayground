248ad97e40fe32ba8276777d8c3c9d8a37215e9d
Patrick Trapp <patrick.trapp@cgi.com>
2020-04-17 18:47:35 +0200

    Textstatistics Lösung - kleiner Bugfix!

--- src/main/java/com/cgi/bootstrap/playground/hashmap/Textstatistics.java
+++ src/main/java/com/cgi/bootstrap/playground/hashmap/Textstatistics.java
@@ -2,12 +2,8 @@ package com.cgi.bootstrap.playground.hashmap;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Textstatistics {
@@ -18,7 +14,9 @@ public class Textstatistics {
         Textstatistics statistics = new Textstatistics("taucher.txt");
 
         Map<String, Integer> wordCount = statistics.wordCount();
+
         wordCount.forEach((k,v)->System.out.println(k+":"+v));
+
     }
 
     public Textstatistics(String fileName){
@@ -35,6 +33,8 @@ public class Textstatistics {
                 .lines()
                 .flatMap( l -> Arrays.stream(l.split("[:\\?,\\.\\-–\\! \\t\\r\\n]")))
                 .filter(w -> !w.isBlank())
-                .collect(Collectors.toMap(w -> w, w -> 1, (k, v)-> v+1));
+                .collect(Collectors.toMap(w -> w, w -> Integer.valueOf(1), (v1, v2)-> v1 + v2));
     }
+
+
 }
