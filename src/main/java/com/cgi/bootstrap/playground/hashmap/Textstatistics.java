4acf8f5cd9071009f32440c443d7ad64f650f7df
Patrick Trapp <patrick.trapp@cgi.com>
2020-04-17 17:22:55 +0200

    Textstatistics Rumpf erzeugt.

--- src/main/java/com/cgi/bootstrap/playground/hashmap/Textstatistics.java
+++ src/main/java/com/cgi/bootstrap/playground/hashmap/Textstatistics.java
@@ -3,6 +3,7 @@ package com.cgi.bootstrap.playground.hashmap;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.Arrays;
 
 public class Textstatistics {
 
@@ -24,11 +25,11 @@ public class Textstatistics {
                 new InputStreamReader(getClass().getClassLoader().getResourceAsStream(fileName))
         );
 
-        reader.lines()
-                .forEach( (l) -> {
-                    System.out.print(l.length() + " - ");
-                    System.out.println(l);
-                } );
+        reader
+                .lines()
+                .flatMap( l -> Arrays.stream(l.split("[:\\?,\\.\\-\\! \\t\\r\\n]")))
+                .filter(w -> !w.isBlank())
+                .forEach(System.out::println);
 
     }
 }
